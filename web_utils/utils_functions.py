74b4b0b13e5d4cff4c60793e0be8ef8816a1e179
Adam <112779331+Tapcio@users.noreply.github.com>
2023-07-16 19:43:04 +0100

    Add files via upload

---
+++ web_utils/utils_functions.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import joblib
+from scraping import cars_data_cleanup_functions
+
+cars = cars_data_cleanup_functions.pre_eda_car_df()
+loaded_model = joblib.load("model_fitting/final_car_price_prediction_model.pkl")
+columns = joblib.load("model_fitting/column_names.pkl")
+
+
+def update_output(
+    n_clicks, mileage, year, engine_size, drive_type, equipment, make, model
+):
+    if None in [mileage, year, engine_size, drive_type, equipment, make, model]:
+        return ""
+
+    values_for_prediction = []
+
+    values_for_prediction.append(int(year))
+    values_for_prediction.append(mileage)
+    values_for_prediction.append(engine_size)
+
+    if equipment == "Luxury":
+        values_for_prediction.extend([1, 1, 1, 1, 1, 1, 1])
+    elif equipment == "Good":
+        values_for_prediction.extend([1, 1, 0, 0, 1, 1, 1])
+    elif equipment == "Medium":
+        values_for_prediction.extend([0, 1, 0, 0, 1, 0, 1])
+    else:
+        values_for_prediction.extend([0, 0, 0, 0, 0, 0, 0])
+
+    if drive_type == "4WD":
+        values_for_prediction.extend([0, 0, 1])
+    elif drive_type == "FWD":
+        values_for_prediction.extend([1, 0, 0])
+    else:
+        values_for_prediction.extend([0, 1, 0])
+
+    segment = cars.loc[cars["model"] == model, "segment"].values[0]
+
+    if segment == "C":
+        values_for_prediction.extend([1, 0, 0, 0, 0, 0, 0])
+    elif segment == "D":
+        values_for_prediction.extend([0, 1, 0, 0, 0, 0, 0])
+    elif segment == "E":
+        values_for_prediction.extend([0, 0, 1, 0, 0, 0, 0])
+    elif segment == "F":
+        values_for_prediction.extend([0, 0, 0, 1, 0, 0, 0])
+    elif segment == "J-High":
+        values_for_prediction.extend([0, 0, 0, 0, 1, 0, 0])
+    elif segment == "J-Low":
+        values_for_prediction.extend([0, 0, 0, 0, 0, 1, 0])
+    else:
+        values_for_prediction.extend([0, 0, 0, 0, 0, 0, 1])
+
+    df = pd.DataFrame(data=[values_for_prediction], columns=columns)
+    prediction = loaded_model.predict(df)
+    price = round(prediction[0], 2)
+    return f"Car Price: ${price}"
---
+++ web_utils/utils_input.py
@@ -0,0 +1,135 @@
+import numpy as np
+import dash_bootstrap_components as dbc
+from dash import dcc, html
+from scraping import cars_data_cleanup_functions
+
+cars = cars_data_cleanup_functions.pre_eda_car_df()
+
+row1 = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H6("Make"),
+                dcc.Dropdown(
+                    id="make-dropdown",
+                    options=[
+                        {"label": make, "value": make} for make in cars["name"].unique()
+                    ],
+                    style={"margin-bottom": "10px"},
+                ),
+            ],
+            className="col-sm",
+        ),
+        dbc.Col(
+            [
+                html.H6("Model"),
+                dcc.Dropdown(
+                    id="model-dropdown",
+                    className="dropdown-class",
+                    style={"margin-bottom": "10px"},
+                ),
+            ]
+        ),
+    ],
+    class_name="row sm-2",
+)
+
+row2 = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H6("Year"),
+                dcc.Dropdown(
+                    id="year-dropdown",
+                    options=[
+                        {"label": str(year), "value": year}
+                        for year in np.sort(cars["year"].unique())
+                    ],
+                    style={"margin-bottom": "10px"},
+                ),
+            ]
+        ),
+        dbc.Col(
+            [
+                html.H6("Mileage"),
+                dcc.Slider(
+                    id="mileage-slider",
+                    min=0,
+                    max=300000,
+                    value=0,
+                ),
+            ]
+        ),
+    ],
+    className="mb-2",
+)
+
+row3 = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H6("Engine"),
+                dcc.Dropdown(
+                    id="engine-dropdown",
+                    options=[
+                        {"label": str(engine_size), "value": engine_size}
+                        for engine_size in np.sort(cars["engine_description"].unique())
+                    ],
+                    style={"margin-bottom": "10px"},
+                ),
+            ]
+        ),
+        dbc.Col(
+            [
+                html.H6("Drive Type"),
+                dcc.Dropdown(
+                    id="drive-type-dropdown",
+                    options=[
+                        {"label": drive_type, "value": drive_type}
+                        for drive_type in ["4WD", "FWD", "RWD"]
+                    ],
+                    style={"margin-bottom": "10px"},
+                ),
+            ]
+        ),
+    ]
+)
+
+row4 = dbc.Row(
+    [
+        dbc.Col(
+            [
+                html.H6("Equipment"),
+                dcc.Dropdown(
+                    id="equipment-level-dropdown",
+                    options=[
+                        {"label": equipment, "value": equipment}
+                        for equipment in ["Luxury", "Good", "Medium", "Low"]
+                    ],
+                    style={"margin-bottom": "10px"},
+                ),
+            ]
+        ),
+        dbc.Col(
+            [
+                html.Div(
+                    dbc.Button(
+                        "Predict Price",
+                        className="me-1",
+                        id="submit-button-state",
+                        color="dark",
+                        style={"width": "250px"},
+                    ),
+                    className="d-flex justify-content-center",
+                    style={
+                        "display": "inline-block",
+                        "margin-top": "25px",
+                        "margin-left": "200px",
+                        "verticalAlign": "middle",
+                    },
+                ),
+            ],
+            className="d-flex align-items-center",
+        ),
+    ]
+)
